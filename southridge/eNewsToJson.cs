9af80df3e55b5e989ebcf67abb453cd421e0fcd9
Kristofer Krause <kris.krause@gmail.com>
2013-03-22 09:44:01 -0400

    palm sunday enews

--- southridge/eNewsToJson.cs
+++ southridge/eNewsToJson.cs
@@ -9,7 +9,7 @@ public class MyClass
 	{
 		try
         {	
-			var passage = "Mark 14:32-42";
+			var passage = "Mark 15:1-15";
 			var file = @"C:\Users\Krause\Documents\GitHub\dragthor.github.com\southridge\eNews.txt";
 			var outFile = @"C:\Users\Krause\Documents\GitHub\dragthor.github.com\southridge\eNews.json";
 			var result = new StringBuilder();
