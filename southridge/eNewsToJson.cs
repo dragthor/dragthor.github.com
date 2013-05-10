38373259bd8ac660bd168a2e86a326f56a09fd62
Kristofer Krause <kris.krause@gmail.com>
2013-05-10 14:34:15 -0400

    enews update

--- southridge/eNewsToJson.cs
+++ southridge/eNewsToJson.cs
@@ -9,7 +9,7 @@ public class MyClass
 	{
 		try
         {	
-			var passage = "Mark 5:24-34";
+			var passage = "Mark 6:1-6";
 			var file = @"C:\Users\Krause\Documents\GitHub\dragthor.github.com\southridge\eNews.txt";
 			var outFile = @"C:\Users\Krause\Documents\GitHub\dragthor.github.com\southridge\eNews.json";
 			var result = new StringBuilder();
