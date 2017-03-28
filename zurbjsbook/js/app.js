ab9362b75d4fbc5437dcc75dda1cbabf9de010c6
Kristofer Krause <kris.krause@gmail.com>
2017-03-28 06:02:09 -0400

    save

--- zurbjsbook/js/app.js
+++ zurbjsbook/js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation()
+
+Foundation.version; // "6.3.1"
+
+Foundation.rtl(); // false
+
+Foundation.GetYoDigits(); // "2jrpcg"
+
+Foundation.getFnName(function() {}); // ""
+
+
+
+
+Foundation.util.throttle(function() {}, 2 * 1000);
